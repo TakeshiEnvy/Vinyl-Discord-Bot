3f1ed2cae3610f45dd7ad0435d1ff17da8370ed2
TakeshiEnvy <v_nguyenvu@outlook.com>
2021-09-16 00:03:36 +0700

    Minor updates

--- message.py
+++ message.py
@@ -9,14 +9,14 @@ class message(commands.Cog):
 
     #get inspired quote from "zenquotes"
     def zenquotes(self):
-        response = requests.get("https://zenquotes.io/api/random")
+        response = requests.get("https://zenquotes.io/api/random", timeout=3)
         json_data = json.loads(response.text)
         quote = f"'*{json_data[0]['q']}*'**  -  **`{json_data[0]['a']}`"
         return quote
     
     #get insult from "Evil Insult"
     def evilinsult(self):
-        response = requests.get("https://evilinsult.com/generate_insult.php?lang=en&type=json")
+        response = requests.get("https://evilinsult.com/generate_insult.php?lang=en&type=json", timeout=3)
         json_data = json.loads(response.text)
         return json_data['insult']
 
@@ -30,12 +30,12 @@ class message(commands.Cog):
         quote = self.evilinsult()
         await ctx.send(f'😈 : {quote}')
     
-    @commands.command(name='user', help='Display info of tagged person', aliases=['info'])
+    @commands.command(name='user', help='Display info of tagged person')
     async def user(self, ctx, member: discord.Member):
-        embedVar = discord.Embed(title= member.name, description=member.mention, color= discord.Color.green())
-        embedVar.add_field(name= "ID", value= member.id, inline= True)
-        embedVar.set_thumbnail(url= member.avatar_url)
-        embedVar.set_footer(icon_url= ctx.author.avatar_url, text= f"Requested by {ctx.author.name}")
+        embedVar = discord.Embed(title=member.name, description=member.mention, color=discord.Color.green())
+        embedVar.add_field(name="ID", value=member.id, inline= True)
+        embedVar.set_thumbnail(url=member.avatar_url)
+        embedVar.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
         await ctx.send(embed=embedVar)
 
 def setup(bot):
